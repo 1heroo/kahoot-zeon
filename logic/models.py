6bbfa79d318b6503b8308eb1955970b8e2eb2279
1heroo <106371215+1heroo@users.noreply.github.com>
2022-09-06 19:27:52 +0600

    renamed model

--- logic/models.py
+++ logic/models.py
@@ -15,8 +15,6 @@ class Player(AbstractUser):
         return f'{self.first_name} {self.last_name}'
 
     class Meta:
-        verbose_name = "User"
-        verbose_name_plural = "Users"
         ordering = ['rank']
 
 
